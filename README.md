d09d63bf7d11d5ccc979f8354507614e7c8cc100
John Armstrong <j.armstrong484@gmail.com>
2015-05-11 11:12:43 +1200

    reinitialised repo to remove history from a seed project

---
+++ README.md
@@ -0,0 +1,40 @@
+# welectricity-ninja-hertz
+
+A barebones Node.js app using [Express 4](http://expressjs.com/).
+
+## Running Locally
+
+Make sure you have [Node.js](http://nodejs.org/) and the [Heroku Toolbelt](https://toolbelt.heroku.com/) installed.
+
+Server address: https://welectricity-ninja-hertz.herokuapp.com/
+
+Use nodeJS and expressJS on index.js
+
+## Clone code from Heroku
+
+Clone code to your local from Heroku:
+$heroku git:clone -a welectricity-ninja-hertz
+
+## Inportant Notes
+
+Format to get response: https://welectricity-ninja-hertz.herokuapp.com/[power_source]/[place_id]/[info]
+
+Example: https://welectricity-ninja-hertz.herokuapp.com/hydropower/0/power
+
+
+Details: 
+[power_source]: oil, gas, coal, unclear, hydropower, genthermal
+
+[place_id] (when u place the power source at different place, the data associated with place was different, like hill, close river, residential area， etc): 0, 1, 2, 3, ...
+
+[info]: cost, power, pollute
+
+## Documentation
+
+For more information about using Node.js on Heroku, see these Dev Center articles:
+
+- [Getting Started with Node.js on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs)
+- [Heroku Node.js Support](https://devcenter.heroku.com/articles/nodejs-support)
+- [Node.js on Heroku](https://devcenter.heroku.com/categories/nodejs)
+- [Best Practices for Node.js Development](https://devcenter.heroku.com/articles/node-best-practices)
+- [Using WebSockets on Heroku with Node.js](https://devcenter.heroku.com/articles/node-websockets)
---
+++ index.js
@@ -0,0 +1,204 @@
+//Application: welectricity-ninja-hertz
+//Author: Tianfu Yuan
+
+var express = require('express');
+
+// added cors
+var app = express()
+   , cors = require('cors');
+
+app.use(express.bodyParser());
+app.use(express.static(__dirname));
+app.use(cors());
+
+//data
+var hydropower = [
+	{power: '2000', cost: '200', pollute: '200'},
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''}
+];
+
+var coal = [
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''}
+];
+
+var oil = [
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''}
+];
+
+var gas = [
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''}
+];
+
+var genthermal = [
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''}
+];
+
+var unclear = [
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''},
+	{power: '', cost: '', pollute: ''}
+];
+
+var server = app.listen(process.env.PORT, function() {
+    console.log('Listening on port %d', server.address().port);
+});
+
+//GET hydropower
+app.get('/hydropower/:id/power', function(req, res) {
+  if(hydropower.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(hydropower[req.params.id].power);
+});
+
+app.get('/hydropower/:id/cost', function(req, res) {
+  if(hydropower.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(hydropower[req.params.id].cost);
+});
+
+app.get('/hydropower/:id/pollute', function(req, res) {
+  if(hydropower.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(hydropower[req.params.id].pollute);
+});
+
+//GET coal
+app.get('/coal/:id/power', function(req, res) {
+  if(coal.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(coal[req.params.id].power);
+});
+
+app.get('/coal/:id/cost', function(req, res) {
+  if(coal.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(coal[req.params.id].cost);
+});
+
+app.get('/coal/:id/pollute', function(req, res) {
+  if(coal.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(coal[req.params.id].pollute);
+});
+
+//GET oil
+app.get('/oil/:id/power', function(req, res) {
+  if(oil.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(oil[req.params.id].power);
+});
+
+app.get('/oil/:id/cost', function(req, res) {
+  if(oil.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(oil[req.params.id].cost);
+});
+
+app.get('/oil/:id/pollute', function(req, res) {
+  if(oil.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(oil[req.params.id].pollute);
+});
+
+//GET gas
+app.get('/gas/:id/power', function(req, res) {
+  if(gas.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(gas[req.params.id].power);
+});
+
+app.get('/gas/:id/cost', function(req, res) {
+  if(gas.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(gas[req.params.id].cost);
+});
+
+app.get('/gas/:id/pollute', function(req, res) {
+  if(gas.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(gas[req.params.id].pollute);
+});
+
+//GET genthermal
+app.get('/genthermal/:id/power', function(req, res) {
+  if(genthermal.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(genthermal[req.params.id].power);
+});
+
+app.get('/genthermal/:id/cost', function(req, res) {
+  if(genthermal.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(genthermal[req.params.id].cost);
+});
+
+app.get('/genthermal/:id/pollute', function(req, res) {
+  if(genthermal.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(genthermal[req.params.id].pollute);
+});
+
+//GET nuclear
+app.get('/nuclear/:id/power', function(req, res) {
+  if(nuclear.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(nuclear[req.params.id].power);
+});
+
+app.get('/nuclear/:id/cost', function(req, res) {
+  if(nuclear.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(nuclear[req.params.id].cost);
+});
+
+app.get('/nuclear/:id/pollute', function(req, res) {
+  if(nuclear.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(nuclear[req.params.id].pollute);
+});
+
