459eb3bbab0f22838d1b4b4295589da451421c86
Tianfu Yuan <yuantian@cafe-sardine.ecs.vuw.ac.nz>
2015-05-20 23:02:03 +1200

    add data to every power source

--- index.js
+++ index.js
@@ -16,57 +16,94 @@ app.use(cors());
 //(1) close to river
 //(2) close to residential area
 //(3) close to valley
+//Power as nuclear standard(100), cost as nuclear standard(100), pollute as coal standard(100)
+
+//Source: http://en.wikiversity.org/wiki/Power_Generation-comparison
+//http://www.srpnet.com/environment/earthwise/pdfx/spp/March3/6EPRI.pdf
+
+var wind = [
+	{power: '15', cost: '63', pollute: '10'},
+	{power: '15', cost: '53', pollute: '12'},
+	{power: '15', cost: '68', pollute: '12'},
+	{power: '15', cost: '58', pollute: '12'}
+];
+
 var hydropower = [
-	{power: '2000', cost: '200', pollute: '200'},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''}
+	{power: '12', cost: '60', pollute: '3'},
+	{power: '12', cost: '50', pollute: '2'},
+	{power: '12', cost: '65', pollute: '3'},
+	{power: '12', cost: '55', pollute: '3'}
 ];
 
 var coal = [
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''}
+	{power: '60', cost: '35', pollute: '100'},
+	{power: '60', cost: '26', pollute: '100'},
+	{power: '60', cost: '30', pollute: '100'},
+	{power: '60', cost: '28', pollute: '100'}
 ];
 
 var oil = [
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''}
+	{power: '55', cost: '40', pollute: '87'},
+	{power: '55', cost: '31', pollute: '88'},
+	{power: '55', cost: '35', pollute: '86'},
+	{power: '55', cost: '33', pollute: '88'}
 ];
 
 var gas = [
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''}
+	{power: '40', cost: '45', pollute: '51'},
+	{power: '40', cost: '36', pollute: '50'},
+	{power: '40', cost: '40', pollute: '50'},
+	{power: '40', cost: '28', pollute: '51'}
 ];
 
 var genthermal = [
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''}
+	{power: '35', cost: '78', pollute: '40'},
+	{power: '35', cost: '68', pollute: '41'},
+	{power: '35', cost: '97', pollute: '42'},
+	{power: '35', cost: '88', pollute: '40'}
 ];
 
 var unclear = [
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''}
+	{power: '100', cost: '90', pollute: '16'},
+	{power: '100', cost: '90', pollute: '17'},
+	{power: '100', cost: '100', pollute: '16'},
+	{power: '100', cost: '90', pollute: '17'}
 ];
 
 var solar = [
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''},
-	{power: '', cost: '', pollute: ''}
+	{power: '2', cost: '75', pollute: '42'},
+	{power: '2', cost: '65', pollute: '41'},
+	{power: '2', cost: '95', pollute: '41'},
+	{power: '2', cost: '85', pollute: '40'}
 ];
 
 var server = app.listen(process.env.PORT, function() {
     console.log('Listening on port %d', server.address().port);
+});
+
+//GET wind
+app.get('/wind/:id/power', function(req, res) {
+  if(wind.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(wind[req.params.id].power);
+});
+
+app.get('/wind/:id/cost', function(req, res) {
+  if(wind.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(wind[req.params.id].cost);
+});
+
+app.get('/wind/:id/pollute', function(req, res) {
+  if(wind.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No data found');
+  }
+  res.send(wind[req.params.id].pollute);
 });
 
 //GET hydropower
