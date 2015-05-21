d6b0888c73d15e74c59dc3202799df9966bf18c3
John Armstrong <j.armstrong484@gmail.com>
2015-05-22 10:00:29 +1200

    Now checks to see if there is a game that already exists for the user and either updates it or creates it based on whether it exists

--- index.js
+++ index.js
@@ -30,18 +30,41 @@ app.post('/game', function (req, res) {
         var game = req.body;
 
         //TODO validate the saved game. i.e user_id exists.
-        var saveGameQuery = client.query('INSERT INTO games', [game.user_id, game.population, game.pollution, game.power_demand, game.plants]);
 
-        saveGameQuery.on('end', function (result) {
-            client.end();
-            res.statusCode = 200;
-            res.send('Game saved successfully');
-        });
+        var gameExistsQuery = client.query('SELECT COUNT(*) FROM games WHERE user_id = $1', [req.body.user_id]);
 
-        saveGameQuery.on('error', function (error) {
-            client.end();
-            res.statusCode = 500;
-            res.send('Error 500: An unexpected error has occurred. Details: ' + error);
+        gameExistsQuery.on('end', function(results) {
+            if(results.rows.length > 0) {
+                //A save game for this user does exist so UPDATE it
+                var updateSave = client.query('UPDATE games SET population=$1, power_demand=$2, plants=$3 WHERE user_id=$4', [game.population, game.pollution, game.power_demand, game.plants, game.user_id]);
+
+                updateSave.on('end', function (result) {
+                    client.end();
+                    res.statusCode = 200;
+                    res.send('Game saved successfully');
+                });
+
+                updateSave.on('error', function (error) {
+                    client.end();
+                    res.statusCode = 500;
+                    res.send('Error 500: An unexpected error has occurred. Details: ' + error);
+                });
+            } else {
+                //A save game for this user does not exist so INSERT it
+                var createSave = client.query('INSERT INTO games', [game.user_id, game.population, game.pollution, game.power_demand, game.plants]);
+
+                createSave.on('end', function (result) {
+                    client.end();
+                    res.statusCode = 200;
+                    res.send('Game saved successfully');
+                });
+
+                createSave.on('error', function (error) {
+                    client.end();
+                    res.statusCode = 500;
+                    res.send('Error 500: An unexpected error has occurred. Details: ' + error);
+                });
+            }
         });
     } else {
         res.statusCode = 400;
