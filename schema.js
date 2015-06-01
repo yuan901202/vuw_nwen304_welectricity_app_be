8cfceace7c49d5b4f0f84e95e034795216813572
John Armstrong <j.armstrong484@gmail.com>
2015-06-01 19:02:07 +1200

    Now creates a user table to store all user information

--- schema.js
+++ schema.js
@@ -3,19 +3,28 @@
  */
 var pg = require('pg').native,
     connectionString = process.env.DATABASE_URL,    //This is set in the heroku environment
-    client,
-    query;
+    client;
 
 client = new pg.Client(connectionString);
 client.connect();
 
 var dropQuery = client.query('DROP TABLE IF EXISTS games');
 
+//Create a user table to store all user info
+var createUserQuery = client.query('CREATE TABLE users (user_id SERIAL PRIMARY KEY, username VARCHAR(255), password text NOT NULL');
+
 //Create a new saved game table
-query = client.query('CREATE TABLE games (user_id integer UNIQUE, population integer, pollution integer, power_demand integer, plants integer[])');
+var savedGamesQuery = client.query('CREATE TABLE games (user_id integer UNIQUE, population integer, pollution integer, power_demand integer, plants integer[])');
+
 //TODO add user_id as a foreign key when we create a user table
 
-query.on('end', function (result) {
+createUserQuery.on('end', function (result) {
+    console.log('User table created');
+});
+
+savedGamesQuery.on('end', function (result) {
     client.end();
     console.log('Table games created');
 });
+
+//Figure out what to do on errors. eg restart server
